2240511d8ba6f6b8cd8059c9f4c91a4d9b51c134
dev-Sara <mkmobasara.dev@gmail.com>
2023-06-28 14:34:54 +0600

    init commit

---
+++ lib/music_player.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/screen/audio_player_screen.dart';
+
+class MusicPlayer extends StatelessWidget {
+  const MusicPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Music Player',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MusicPlayerScreen(),
+    );
+  }
+}
